483ac089c07431d7a06ca58d6da167e7724d9de2
BYBLIK-C-MALINOY <morty0472@gmail.com>
2025-03-30 23:24:09 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import { logAllQuestions, createSurvey } from "./database/queries";
 
 export async function main() {
   try {
-    const connection = getConnection()
+   const connection = getConnection()
     await logAllQuestions(connection)
+    await getTop5Countries(connection)
+    await getMentalHealthStats(connection)
+    await getPhysicalOverMentalHealth(connection)
+    await getTop20Words(connection)
     await createSurvey(connection, { Description: "Insertable Test" })
   } catch (error) {
     console.error("Failed to start:", error);
